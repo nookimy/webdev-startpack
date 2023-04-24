a8ce578759d32c30ae4e529b54555a0c6ba121cd
Nookimy <nookimy@gmail.com>
2023-04-24 15:17:23 +0300

    Оптимизация картинок. Создание WEBP

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -8,18 +8,22 @@ const srcFolder = `./src`;
 export const path = {
     build: {
         js: `${buildFolder}/js/`,
+        images: `${buildFolder}/img/`,
         css: `${buildFolder}/css/`,
         html: `${buildFolder}/`,
         files: `${buildFolder}/files/`,
     },
     src: {
         js: `${srcFolder}/js/app.js`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        svg: `${srcFolder}/img/**/*.svg`,
         scss: `${srcFolder}/scss/style.scss`,
         html: `${srcFolder}/*.pug`,
         files: `${srcFolder}/files/**/*.*`,
     },
     watch: {
         js: `${srcFolder}/scss/**/*.js`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg}`,
         scss: `${srcFolder}/scss/**/*.scss`,
         html: `${srcFolder}/**/*.pug`,
         files: `${srcFolder}/files/**/*.*`,
