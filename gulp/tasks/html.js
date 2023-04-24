37b596ef893f6085b380093be79453db6ca8450a
Nookimy <nookimy@gmail.com>
2023-04-24 12:28:54 +0300

    Обработка HTML/PUG файлов

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,6 +1,7 @@
 import fileInclude from "gulp-file-include";
 import webpHtmlNosvg from "gulp-webp-html-nosvg";
 import versionNumber from "gulp-version-number";
+import pug from "gulp-pug";
 
 export const html = () => {
     return app.gulp.src(app.path.src.html)
@@ -10,7 +11,13 @@ export const html = () => {
                 message: "Error: <%= error.message %>"
             }))
         )
-        .pipe(fileInclude())
+        .pipe(pug({
+            // сжатие html-файла
+            pretty: true,
+            // Показывать в терминале какой файл обработан
+            verbose: true
+
+        }))
         .pipe(app.plugins.replace(/@img\//g, 'img/'))
         .pipe(webpHtmlNosvg())
         .pipe(
