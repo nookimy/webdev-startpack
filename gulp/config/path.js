a62c1b57210e59abbb6c84c0cab1f749612b863d
Nookimy <nookimy@gmail.com>
2023-04-24 14:39:06 +0300

    Обработка JavaScript файлов. WEBPACK

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,16 +7,19 @@ const srcFolder = `./src`;
 
 export const path = {
     build: {
+        js: `${buildFolder}/js/`,
         css: `${buildFolder}/css/`,
         html: `${buildFolder}/`,
         files: `${buildFolder}/files/`,
     },
     src: {
+        js: `${srcFolder}/js/app.js`,
         scss: `${srcFolder}/scss/style.scss`,
         html: `${srcFolder}/*.pug`,
         files: `${srcFolder}/files/**/*.*`,
     },
     watch: {
+        js: `${srcFolder}/scss/**/*.js`,
         scss: `${srcFolder}/scss/**/*.scss`,
         html: `${srcFolder}/**/*.pug`,
         files: `${srcFolder}/files/**/*.*`,
