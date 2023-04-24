dd3f162c20394b8e0c7689902e034e0c7f350167
Nookimy <nookimy@gmail.com>
2023-04-24 14:00:06 +0300

    Обработка SASS/SCSS/CSS файлов

---
+++ gulp/tasks/scss.js
@@ -0,0 +1,40 @@
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import rename from 'gulp-rename';
+
+import cleanCss from 'gulp-clean-css'; // Сжатие CSS-файла
+import webpCss from 'gulp-webpcss'; // Вывод webp-изображений
+import autoprefixer from 'gulp-autoprefixer'; // Добавление вендорных префиксов
+import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Группировка медиа-запросов
+
+const sass = gulpSass(dartSass);
+
+export const scss = () => {
+    return app.gulp.src(app.path.src.scss, { sourcemaps: true })
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "SCSS",
+                message: "Error: <%= error.message %>"
+            })))
+        .pipe(app.plugins.replace(/@img\//g, '../img/'))
+        .pipe(sass({
+           outputStyle: 'expanded'
+        }))
+        .pipe(groupCssMediaQueries())
+        .pipe(webpCss({
+            webpClass: ".webp",
+            noWebpClass: ".no-webp"
+        }))
+        .pipe(autoprefixer({
+            grid: true,
+            overrideBrowserlist: ["last 3 versions"],
+            cascade: true
+        }))
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(cleanCss())
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(app.gulp.dest(app.path.build.css))
+        .pipe(app.plugins.browsersync.stream());
+}
