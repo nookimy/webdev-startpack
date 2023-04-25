d66faf3c61a7ab9cb44694007cb4c86cc9580d93
Nookimy <nookimy@gmail.com>
2023-04-25 13:27:12 +0300

    Выгрузка результата на сервер по FTP

--- gulpfile.js
+++ gulpfile.js
@@ -26,6 +26,7 @@ import  { images } from "./gulp/tasks/images.js";
 import  { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 import  { svgSprive } from "./gulp/tasks/svgSprive.js";
 import  { zip } from "./gulp/tasks/zip.js";
+import  { ftp } from "./gulp/tasks/ftp.js";
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
@@ -49,11 +50,13 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
 
 //Экспорт сценариев
 export { dev }
 export  { build }
 export  { deployZIP }
+export  { deployFTP }
 
 //Выполнение сценария по умолчанию
 gulp.task('default', dev);
